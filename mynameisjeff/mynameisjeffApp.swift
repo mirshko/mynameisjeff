5b3667d58304e20bbcfe1ea38547a9575694c5cc
Jeff Reiner <gone@hey.com>
2020-08-15 13:44:09 +0200

    Initial Commit

---
+++ mynameisjeff/mynameisjeffApp.swift
@@ -0,0 +1,17 @@
+//
+//  mynameisjeffApp.swift
+//  mynameisjeff
+//
+//  Created by Jeffrey Reiner on 8/15/20.
+//
+
+import SwiftUI
+
+@main
+struct mynameisjeffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
