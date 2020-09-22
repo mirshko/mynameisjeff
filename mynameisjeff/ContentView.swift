015e778253f57f109579b48c482deaa6e7b37d70
Jeff Reiner <gone@hey.com>
2020-09-22 23:47:13 +0200

    Working on something

--- mynameisjeff/ContentView.swift
+++ mynameisjeff/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetails = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Jeff").font(.largeTitle).bold().padding()
+            
+            Button(action: {
+                let generator = UINotificationFeedbackGenerator()
+                
+                generator.notificationOccurred(.success)
+                
+                self.showDetails.toggle()
+            }) {
+                Image("SwagOnMax")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 180.0, height: 180.0)
+                    .clipShape(RoundedRectangle(cornerRadius: (10/57 * 180.0), style: .continuous))
+            }
+            
+            Text("@mirshko")
+                .font(.title3)
+                .padding()
+                .scaleEffect(showDetails ? 1 : 0.92)
+                .opacity(showDetails ? 1 : 0)
+                .animation(.easeIn(duration: 0.150))
+        }
     }
 }
 
