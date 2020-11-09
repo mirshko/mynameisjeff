73afea29494b464e19158985f8c19395ddf4eb1f
Jeff Reiner <gone@hey.com>
2020-11-09 22:17:21 +0100

    Working on new design

--- mynameisjeff/ContentView.swift
+++ mynameisjeff/ContentView.swift
@@ -7,14 +7,41 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var showDetails = false
+struct Section: View {
+    var text: String
     
     var body: some View {
+        HStack() {
+            Text(text)
+            Spacer()
+        }
+        .font(.headline)
+        .padding()
+        .background(Color.white)
+        .foregroundColor(.black)
+        .cornerRadius(16)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
         VStack {
-            Link(destination: URL(string: "https://twitter.com/mirshko")!, label: {
-                Text("JEFF").font(.largeTitle).bold().foregroundColor(.primary)
-            })
+            ScrollView() {
+                Section(text: "Jeff Reiner")
+                
+                Section(text: "I'm currently freelancing as a design engineer located in Berlin, as well as working on frontend at Union and design at Soapbox.")
+                
+                Section(text: "I focus on engaging, responsive, mobile-first websites and apps; highly-usable and efficient design systems; and solving complex UX problems, in and around the Web 2.0 and Web3 space.")
+                
+                
+                
+            }.cornerRadius(16)
+            
+            HStack(alignment: .center) {
+                Link(destination: URL(string: "https://twitter.com/mirshko")!, label: {
+                    Text("JEFF").font(.largeTitle).bold().foregroundColor(.primary)
+                })
+            }.padding()
         }
     }
 }
