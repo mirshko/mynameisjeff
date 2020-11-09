fd9c075e49bc195203dd029aee83955c0be06bc5
Jeff Reiner <gone@hey.com>
2020-11-09 20:31:37 +0100

    Cleaning up the homepage

--- mynameisjeff/ContentView.swift
+++ mynameisjeff/ContentView.swift
@@ -12,34 +12,15 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("Jeff").font(.largeTitle).bold().padding()
-            
-            Button(action: {
-                let generator = UINotificationFeedbackGenerator()
-                
-                generator.notificationOccurred(.success)
-                
-                self.showDetails.toggle()
-            }) {
-                Image("SwagOnMax")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 180.0, height: 180.0)
-                    .clipShape(RoundedRectangle(cornerRadius: (10/57 * 180.0), style: .continuous))
-            }
-            
-            Text("@mirshko")
-                .font(.title3)
-                .padding()
-                .scaleEffect(showDetails ? 1 : 0.92)
-                .opacity(showDetails ? 1 : 0)
-                .animation(.easeIn(duration: 0.150))
+            Link(destination: URL(string: "https://twitter.com/mirshko")!, label: {
+                Text("JEFF").font(.largeTitle).bold().foregroundColor(.primary)
+            })
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().preferredColorScheme(.dark)
     }
 }
